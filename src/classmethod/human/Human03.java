73e9ef50de11bff2a36ccc4d21442f68ad2c8bb9
katsuma0728 <katsuma20220728@gmail.com>
2023-09-15 00:33:11 +0900

    [Add]

---
+++ src/classmethod/human/Human03.java
@@ -0,0 +1,28 @@
+package classmethod.human;
+
+public class Human03 {
+//	メンバー変数
+	public String name;
+	public int age;
+	public String profession;
+	
+	public Human03() {
+		this.name = "山田";
+		this.age = 20;
+		this.profession = "プログラマー";
+	}
+	
+//	String型の戻り値を返す引数なしのメゾッドを定義
+	public String getProfile() {
+		return "年齢は" + this.age + "職業は" + this.profession;
+	}
+	
+	
+	public void greet(String friend) {
+		if (friend == null) {
+			System.out.println("挨拶する友達がわかりません！");
+			return;
+		}
+		System.out.println(friend + "さん、こんにちは！");
+	}
+}
